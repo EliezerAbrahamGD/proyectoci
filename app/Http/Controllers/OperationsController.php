624365ba1bbf43345c750bad1353ab7b22788292
thevato98 <eliezer9871@gmail.com>
2026-05-22 16:46:29 -0600

    EliezerPart

--- app/Http/Controllers/OperationsController.php
+++ app/Http/Controllers/OperationsController.php
@@ -4,10 +4,8 @@ namespace App\Http\Controllers;
 
 class OperationsController extends Controller
 {
-
     public function power(int $a, int $b): int|float
     {
         return pow($a, $b);
     }
 }
-
